d974c171e6951a257b8fddaec30e65cd4bd09e9d
y1jay <67768207+y1jay@users.noreply.github.com>
2020-07-14 22:11:12 +0900

    집에서

--- employee/app.js
+++ employee/app.js
@@ -29,14 +29,15 @@ request.get({url : baseUrl+query, json:true},(error,response,body)=>{
    let salary = data.employee_salary
    let age = data.employee_age
 
-   let new_data = ('('+id+','+'"'+name+'"'+','+salary+','+age+')'+',')
+   let new_data = ('('+id+','+'"'+name+'"'+','+salary+','+age+','+'default'+')')
    console.log(new_data)
-   for(let i = 0; i<new_data.length; i++){
-    let insert_query = 'insert into employee(id,emproyee_name,employee_age,employee_salaray,profile_image) values'+new_data +'default'+';'
+
+    let insert_query = 'insert into employee values'+new_data+';'
     connection.query(insert_query,(error,res,field)=>{
-        // console.log(res)
+        console.log(res)
+        // console.log(insert_query)
     })
-    }
+    
 }
 })
 
