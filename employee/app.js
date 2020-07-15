cc14d6a4cdb49e22055d9a6cb421abe6b30d3fc1
y1jay <67768207+y1jay@users.noreply.github.com>
2020-07-15 11:56:52 +0900

    api 인서트 최종본

--- employee/app.js
+++ employee/app.js
@@ -1,9 +1,9 @@
 const request = require('postman-request');
 
 
-const baseUrl ='http://dummy.restapiexample.com/api/v1/'
+const baseUrl ='http://dummy.restapiexample.com'
 
-let query = 'employees'
+let query = '/api/v1/employees'
 
 
 const connection = require('./mysql_connection.js')
@@ -16,29 +16,31 @@ const connection = require('./mysql_connection.js')
 
 
 request.get({url : baseUrl+query, json:true},(error,response,body)=>{
-    
+    let insert_query = `insert into employee values`
     let dataArray = body.data
     // console.log(body.data[0].employee_name)
    for(let i= 0; i< dataArray.length; i++){
     //    let data = dataArray[i]
     
     //    console.log(dataArray[i])
-   let data = dataArray[i]
-   let id = data.id
-   let name = data.employee_name
-   let salary = data.employee_salary
-   let age = data.employee_age
+   let arr = dataArray[i]
 
-   let new_data = ('('+id+','+'"'+name+'"'+','+salary+','+age+','+'default'+')')
-   console.log(new_data)
+   let id = arr.id
+   let name = arr.employee_name
+   let age = arr.employee_age
+   let salary = arr.employee_salary
+   
+//    console.log(new_data)
+   insert_query = insert_query+`(${id},"${name}",${age},${salary}),`
+   }
+   insert_query = insert_query.slice(0,-1)
+//    console.log(insert_query.slice(0,-1))
 
-    let insert_query = 'insert into employee values'+new_data+';'
     connection.query(insert_query,(error,res,field)=>{
         console.log(res)
         // console.log(insert_query)
     })
     
-}
+    connection.end()
 })
 
-connection.end()
