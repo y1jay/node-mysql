4159932295fe0ad41be6278bf9f235dbd6f504ec
y1jay <67768207+y1jay@users.noreply.github.com>
2020-07-15 17:11:46 +0900

    줄정리

--- youtube/youtube_app.js
+++ youtube/youtube_app.js
@@ -1,8 +1,9 @@
 const request = require('postman-request');
+const connection = require('./mysql_connection.js')
 
 const url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyDdgxWR7TFIQ7i_lm_LhwgUrIl-iJ5B4v0&maxResults=19&q=킹크랩'
 
-const connection = require('./mysql_connection.js')
+
 
 let encodedUrl = encodeURI(url)
 
@@ -12,17 +13,18 @@ request.get({url : encodedUrl, json:true},(error,response,body)=>{
 //    console.log(youArray)
    
    for(let i = 0; i < youArray.length; i++){
-     let items = youArray[i]
+       let items = youArray[i]
     
 
-    let video_id = items.id.videoId
-    let title = items.snippet.title
-    let channel_title = items.snippet.channelTitle
-    let thumbnail_url = items.snippet.thumbnails.high.url
+       let video_id = items.id.videoId
+       let title = items.snippet.title
+       let channel_title = items.snippet.channelTitle
+       let thumbnail_url = items.snippet.thumbnails.high.url
    
-    // console.log(video_id,title,channel_title,thumbnail_url)
+       // console.log(video_id,title,channel_title,thumbnail_url)
 
-    insert_query = insert_query+`("${video_id}","${title}","${channel_title}","${thumbnail_url}"),`}
+       insert_query = insert_query+`("${video_id}","${title}","${channel_title}","${thumbnail_url}"),`
+    }
 
     insert_query = insert_query.slice(0,-1)
     connection.query(insert_query,(error,res,field)=>{
@@ -32,5 +34,6 @@ request.get({url : encodedUrl, json:true},(error,response,body)=>{
 
     connection.end()
    
-    })
+})
+
 
