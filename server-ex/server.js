edd91725c89b891d7c10ec62c67b322ffcc858bf
y1jay <67768207+y1jay@users.noreply.github.com>
2020-07-16 12:43:21 +0900

    push

--- server-ex/server.js
+++ server-ex/server.js
@@ -8,8 +8,8 @@ dotenv.config({ path: "./config/config.env" });
 // 웹서버 프레임워크인 익스프레스를 가져온다.
 const app = express();
 
-// 환경설정 파일인, config.env파일에 있는 내용을 불러오는 방법.
-const PORT = process.env.PORT;
+// 환경설정 파일인, config.env파일에 있는 내용을 불러오는 방법.(||5000 안써도 되나 안전빵으로 쓰면 좋음)
+const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
